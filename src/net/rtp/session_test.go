2c2ae083413e5ee7c53def005e2eae57798c9320
Anton Kozlov <tonickkozlov@me.com>
2019-08-15 15:52:02 +0100

    Close dataReceiveChan when removing it

---
+++ src/net/rtp/session_test.go
@@ -0,0 +1,51 @@
+package rtp
+
+import (
+  "testing"
+)
+
+func TestCloseSession(t *testing.T) {
+  session := createRecvSession()
+  recv := session.CreateDataReceiveChan()
+  sendTestPacket(session)
+
+  _, ok := <- recv
+
+  if !ok {
+    t.Logf("Did not receive any data")
+    t.FailNow()
+  }
+
+  sendTestPacket(session)
+  session.RemoveDataReceiveChan()
+  assertChannelWasClosed(t, recv)
+}
+
+func assertChannelWasClosed(t *testing.T, recv DataReceiveChan) {
+  select {
+  case _, ok := <- recv:
+    if ok {
+      t.Logf("Received extra data after closing DataReceiveChan")
+      t.FailNow()
+    }
+  default:
+      t.Logf("DataReceiveChan was not closed after RemoveDataReceiveChan was called")
+      t.FailNow()
+  }
+}
+
+func createRecvSession() *Session {
+  // for variable definitions see receive_test.go
+  initSessions()
+  return rsRecv
+}
+
+func sendTestPacket(s *Session) {
+  payload := make([]byte, 160)
+
+  rp := rsRecv.NewDataPacket(160)
+	rp.SetPayload(payload)
+
+	// Feed into receiver session, then check if packet was processed correctly
+	rsRecv.OnRecvData(rp)
+}
