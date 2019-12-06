9c6100aac85b239583603194be8c9455048905c5
Ivan Egorov <vany.egorov@gmail.com>
2019-12-06 12:11:22 +0300

    panic: check PayloadFormatMap key existence in order to prevent panic

--- src/net/rtp/stream.go
+++ src/net/rtp/stream.go
@@ -303,10 +303,12 @@ func (strm *SsrcStream) fillSenderInfo(info senderInfo) {
 	sec, frac := toNtpStamp(tm)
 	info.setNtpTimeStamp(sec, frac)
 
-	tm1 := uint32(tm-strm.initialTime) / 1e6                               // time since session creation in ms
-	tm1 *= uint32(PayloadFormatMap[int(strm.payloadType)].ClockRate / 1e3) // compute number of samples
-	tm1 += strm.initialStamp
-	info.setRtpTimeStamp(tm1)
+	tm1 := uint32(tm-strm.initialTime) / 1e6 // time since session creation in ms
+	if v, ok := PayloadFormatMap[int(strm.payloadType)]; ok {
+		tm1 *= uint32(v.ClockRate / 1e3) // compute number of samples
+		tm1 += strm.initialStamp
+		info.setRtpTimeStamp(tm1)
+	}
 }
 
 // makeSdesChunk creates an SDES chunk at the current inUse position and returns offset that points after the chunk.
